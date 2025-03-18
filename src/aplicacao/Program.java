645adf7724a7b31bafadcc1469673c39fb5dce79
Wenderson Carvalho <wendersonmotarauj@gmail.com>
2025-03-18 20:09:37 -0300

    Projeto JBDC

---
+++ src/aplicacao/Program.java
@@ -0,0 +1,12 @@
+package aplicacao;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department obj = new Department (1, "books");
+		System.out.println(obj);
+	}
+
+}
